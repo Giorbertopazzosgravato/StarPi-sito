454884ab4d228c019410ca2e8d61396e0389472f
Giorbertopazzosgravato <giaggioconsani@gmail.com>
2026-02-03 20:16:53 +0100

    continuato con lo stile del plug alla newsletter

--- src/components/home_page/NewsLetter/IscrizioneNewsLetter.vue
+++ src/components/home_page/NewsLetter/IscrizioneNewsLetter.vue
@@ -5,22 +5,24 @@
 <template>
 	<form action="" class="container" method="post">
 		<h1>Ti piace quello che vedi? Iscriviti alla newsletter!</h1>
-		<input type="text" name="nome" id="nome" class="input txt" placeholder="nome">
-		<input type="email" name="e-mail" id="email" class="input txt" placeholder="e-mail">
-		<button type="submit" class="input btn">Iscrivimi!
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				viewBox="0 0 24 24"
-				width="24"
-				height="24"
-			>
-				<path fill="none" d="M0 0h24v24H0z"></path>
-				<path
-					fill="currentColor"
-					d="M5 13c0-5.088 2.903-9.436 7-11.182C16.097 3.564 19 7.912 19 13c0 .823-.076 1.626-.22 2.403l1.94 1.832a.5.5 0 0 1 .095.603l-2.495 4.575a.5.5 0 0 1-.793.114l-2.234-2.234a1 1 0 0 0-.707-.293H9.414a1 1 0 0 0-.707.293l-2.234 2.234a.5.5 0 0 1-.793-.114l-2.495-4.575a.5.5 0 0 1 .095-.603l1.94-1.832C5.077 14.626 5 13.823 5 13zm1.476 6.696l.817-.817A3 3 0 0 1 9.414 18h5.172a3 3 0 0 1 2.121.879l.817.817.982-1.8-1.1-1.04a2 2 0 0 1-.593-1.82c.124-.664.187-1.345.187-2.036 0-3.87-1.995-7.3-5-8.96C8.995 5.7 7 9.13 7 13c0 .691.063 1.372.187 2.037a2 2 0 0 1-.593 1.82l-1.1 1.039.982 1.8zM12 13a2 2 0 1 1 0-4 2 2 0 0 1 0 4z"
-				></path>
-			</svg>
-		</button>
+		<div>
+			<input type="text" name="nome" id="nome" class="input txt" placeholder="nome">
+			<input type="email" name="e-mail" id="email" class="input txt" placeholder="e-mail">
+			<button type="submit" class="input btn">Iscrivimi!
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 24 24"
+					width="24"
+					height="24"
+				>
+					<path fill="none" d="M0 0h24v24H0z"></path>
+					<path
+						fill="currentColor"
+						d="M5 13c0-5.088 2.903-9.436 7-11.182C16.097 3.564 19 7.912 19 13c0 .823-.076 1.626-.22 2.403l1.94 1.832a.5.5 0 0 1 .095.603l-2.495 4.575a.5.5 0 0 1-.793.114l-2.234-2.234a1 1 0 0 0-.707-.293H9.414a1 1 0 0 0-.707.293l-2.234 2.234a.5.5 0 0 1-.793-.114l-2.495-4.575a.5.5 0 0 1 .095-.603l1.94-1.832C5.077 14.626 5 13.823 5 13zm1.476 6.696l.817-.817A3 3 0 0 1 9.414 18h5.172a3 3 0 0 1 2.121.879l.817.817.982-1.8-1.1-1.04a2 2 0 0 1-.593-1.82c.124-.664.187-1.345.187-2.036 0-3.87-1.995-7.3-5-8.96C8.995 5.7 7 9.13 7 13c0 .691.063 1.372.187 2.037a2 2 0 0 1-.593 1.82l-1.1 1.039.982 1.8zM12 13a2 2 0 1 1 0-4 2 2 0 0 1 0 4z"
+					></path>
+				</svg>
+			</button>
+		</div>
 	</form>
 </template>
 
@@ -35,18 +37,33 @@
 	display: block;
 	margin-left: auto;
 	margin-right: auto;
-	font-size: 1.2em;
+	font-size: 1.3em;
 	border-radius: 10px;
 	border: solid #E16038;
+	background-color: #B8B8B8;
+}
+h1{
+	margin-top: -5%;
+	margin-bottom: 5%;
+	font-size: 3em;
 }
 .txt{
-	width: 100%;
+	width: 80%;
 	height: 5vh;
 	margin-bottom: 5vh;
-
 }
 .btn{
 
+}
+div{
+	background-color: rgba(28, 42, 106, 0.57);
+	padding-top: 3%;
+	padding-bottom: 3%;
+	border-radius: 40px;
+	width: 80%;
+	margin-left: auto;
+	margin-right: auto;
+	transform: translate(0, -5%);
 }
 
 
