9753061f803b8e0a662bbcc08c3fb016079e4d79
AimanDebug <105602441+AimanDebug@users.noreply.github.com>
2026-02-14 21:40:38 +0100

    Iniziato a costruire il footer
    Si vede a malapena ma almeno oggi non sono stato inutile lol.

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <script setup>
 import NavBar from "@/components/common/NavBar.vue";
+import Footer from "@/components/common/Footer.vue";
 </script>
 
 <template>
-	<NavBar/>
+  <NavBar />
 
-	<RouterView/>
+  <RouterView />
 
+  <Footer />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
---
+++ src/components/common/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="footer">
+    <!-- Sponsors -->
+    <div>
+      <div>
+        <h2 class="sponsor-headline">I NOSTRI SPONSOR</h2>
+      </div>
+      <div class="sponsor-logos">
+        <img
+          src="../../../assets/images/logo-dewesoft.jpg"
+          alt="Dewesoft Logo"
+        />
+      </div>
+    </div>
+  </div>
+</template>
