4f38630d1278d8ba3c92803766951d484afdddb9
AimanDebug <105602441+AimanDebug@users.noreply.github.com>
2026-02-15 20:27:24 +0100

    Rifattorizzato il codice per il footer e aggiornato lo stile
    L'header degli sponsor ora ha lo sfondo nero, scritta grigia e font Inter (come su Figma).
    Aggiunto lo stile CSS ad alcune div e testo del footer.

--- src/components/common/Footer.vue
+++ src/components/common/Footer.vue
@@ -1,16 +1,40 @@
+<link rel="stylesheet" href="https://fontshttps://fonts.googleapis.com/css2?family=Inter" />
+
 <template>
   <div class="footer">
     <!-- Sponsors -->
     <div>
-      <div>
+      <div class="sponsor-headline-wrapper">
         <h2 class="sponsor-headline">I NOSTRI SPONSOR</h2>
       </div>
       <div class="sponsor-logos">
-        <img
-          src="../../../assets/images/logo-dewesoft.jpg"
-          alt="Dewesoft Logo"
-        />
+        <img src="../../../assets/images/logo-dewesoft.jpg" alt="Dewesoft Logo" />
       </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+div {
+  width: 100%;
+}
+
+h2 {
+  font-family: 'Inter', sans-serif;
+}
+
+.footer {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.sponsor-headline-wrapper {
+  background-color: #000000;
+}
+
+.sponsor-headline {
+  color: #B2B2B2;
+  text-align: center;
+}
+</style>
