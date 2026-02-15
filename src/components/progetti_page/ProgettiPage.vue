573b2de30cb936ebb11fbd9d444a61ed8590c06e
ScarTheV <riccardo.scaletta05@gmail.com>
2026-02-15 11:13:49 +0100

    Aggiornato README.md

--- src/components/progetti_page/ProgettiPage.vue
+++ src/components/progetti_page/ProgettiPage.vue
@@ -11,7 +11,16 @@
 	<h1>QUESTA PAGINA E' ANCORA WORK-IN-PROGRESS</h1>
 	<p>(non abbiamo progetti da farvi vedere, ci stiamo lavorando)</p>
 
+
+  <button type="button"
+          onclick="document.getElementById('demo').innerHTML = Date()">
+    Click me to display Date and Time.</button>
+
+  <p id="demo"></p>
+
 </template>
+
+
 
 <style scoped>
 
