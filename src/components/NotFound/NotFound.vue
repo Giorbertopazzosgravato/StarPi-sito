cfe85db5f44039ed9b9d4339e6f45ca0c87d992c
ScarTheV <riccardo.scaletta05@gmail.com>
2026-02-24 21:26:30 +0100

    [ Navbar allineato all refresh Navbar allineato all caricamento Non mostra le pagine mancanti Mettesso un 404 Iscrizione al NewsLetter distaziato come le card "/" mappato su "/home" ] ni duovo pk non l'ha preso

---
+++ src/components/NotFound/NotFound.vue
@@ -0,0 +1,24 @@
+<script setup>
+</script>
+
+<template>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <h1> ma che stai a di fra no davvero FR l' URL un c'è </h1>
+
+
+
+  <router-link to="home"> torna alla pagina principale </router-link>
+
+  <p id="demo"></p>
+
+</template>
+
+
+
+<style scoped>
+
+</style>
