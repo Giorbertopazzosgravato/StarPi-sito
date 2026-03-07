c129a9107048f18f550f59ddc92964995c0e5a19
AimanDebug <105602441+AimanDebug@users.noreply.github.com>
2026-03-07 12:09:38 +0100

    Aggiunti loghi degli sponsor al footer, rifattorizzato il componente logo
    Ora i loghi hanno un componente a sé (SponsorLogo).
    Aggiunti i file dei loghi in /static-assets/images.
    Aggiornato il file Footer.vue di conseguenza.
    NOTE: Gli spazi sono per ora assoluti, forse andrebbe cambiato.

--- src/components/common/Footer.vue
+++ src/components/common/Footer.vue
@@ -1,8 +1,36 @@
 <script setup>
-function getSponsorLogos() {
-  // TODO: Fetch sponsor logos from backend API
-  return [];
-}
+import SponsorLogo from "@/components/common/SponsorLogo.vue";
+
+const sponsors = [
+  {
+    src: "../../../static-assets/images/logo-dewesoft.svg",
+    alt: "Logo Dewesoft",
+  },
+  {
+    src: "../../../static-assets/images/logo-nlcomp.svg",
+    alt: "Logo NLComp",
+  },
+  {
+    src: "../../../static-assets/images/logo-unipi.svg",
+    alt: "Logo UniPi",
+  },
+  {
+    src: "../../../static-assets/images/logo-crm.svg",
+    alt: "Logo CRM",
+  },
+  {
+    src: "../../../static-assets/images/logo-ansys.svg",
+    alt: "Logo Ansys",
+  },
+  {
+    src: "../../../static-assets/images/logo-aisler.svg",
+    alt: "Logo Aisler",
+  },
+  {
+    src: "../../../static-assets/images/logo-euroavia.svg",
+    alt: "Logo Euroavia",
+  },
+];
 </script>
 
 <link
@@ -18,7 +46,11 @@ function getSponsorLogos() {
         <h2 class="sponsor-headline">I NOSTRI SPONSOR</h2>
       </div>
       <div class="sponsor-logos">
-        <img v-for="logo in getSponsorLogos()" :src="logo" />
+        <SponsorLogo
+          v-for="sponsor in sponsors"
+          :src="sponsor.src"
+          :alt="sponsor.alt"
+        />
       </div>
     </div>
   </div>
@@ -46,5 +78,15 @@ h2 {
 .sponsor-headline {
   color: #b2b2b2;
   text-align: center;
+  /* TODO: forse è meglio relativo */
+  font-size: 64px;
+}
+
+.sponsor-logos {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  /*TODO: Idem */
+  gap: 120px 100px;
 }
 </style>
---
+++ src/components/common/SponsorLogo.vue
@@ -0,0 +1,16 @@
+<script setup>
+    defineProps({
+        src: {
+            type: String,
+            required: true
+        },
+        alt: {
+            type: String,
+            required: false,
+        }
+    });
+</script>
+
+<template>
+    <img :src="src" :alt="alt" />
+</template>
