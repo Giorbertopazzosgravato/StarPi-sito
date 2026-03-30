c7e3fff905b3f6915d9746b9b20a24be9b5e0926
ScarTheV <riccardo.scaletta05@gmail.com>
2026-03-30 20:45:35 +0200

    sistemata pagina ringrazziamento inscizione al giornale

--- src/components/home_page/NewsLetter/IscrizioneGiornalePagina.vue
+++ src/components/home_page/NewsLetter/IscrizioneGiornalePagina.vue
@@ -1,5 +1,39 @@
 <script setup>
+  import { ref, onMounted } from 'vue'
 
+  const y = ref(0)              // vertical position
+  const countdown = ref(10)      // seconds
+  const isMoving = ref(false)
+
+  function sleep(ms) {
+    return new Promise(r => setTimeout(r, ms))
+  }
+
+  async function startSequence() {
+    // countdown phase
+    while (countdown.value > 0) {
+      await sleep(1000)
+      countdown.value--
+    }
+
+    // start movement
+    isMoving.value = true
+    animate()
+  }
+
+  function animate() {
+    if (y.value > -window.innerHeight + 10) {
+      y.value -= 3 // move up
+      requestAnimationFrame(animate)
+    }
+  }
+
+  onMounted(() => {
+    // start at bottom
+    y.value = window.innerHeight - 900
+
+    startSequence()
+  })
 </script>
 
 <template>
@@ -11,36 +45,70 @@
   <br>
   <br>
 
-  <meta http-equiv="refresh" content="10;url=/">
-
-  <div style="text-align: center; color: antiquewhite">
-  <h1>
-    ! Grazie per esserti iscritto al giornale !
-  </h1>
-  <h1>
-    Da questo momento sarai aggiornato delle ultime novità via mail
-  </h1>
-
-    <h3>
-      Il server stà processando la richiesta
-    </h3>
-
-    <br>
-    <br>
-
-    <div style="text-align: center">
-      <router-link to="/home" style="color: #FF6200"> torna alla pagina principale </router-link>
+  <meta http-equiv="refresh" content="12;url=/">
+  <div class="container">
+    <div style="text-align: center; color: antiquewhite">
+      <h1>
+        ! Grazie per esserti iscritto al giornale !
+      </h1>
+      <h1>
+        Da questo momento sarai aggiornato delle ultime novità via mail
+      </h1>
+      <h3>
+        Il server stà processando la richiesta
+      </h3>
+      <br>
+      <br>
+      <div style="text-align: center">
+        <router-link to="/home" style="color: #FF6200"> torna alla pagina principale </router-link>
+      </div>
     </div>
+
+
+
+
+
+    <!-- countdown display -->
+    <div class="countdown">
+      <h1> {{ countdown }} </h1>
+    </div>
+
+    <!-- image -->
+    <img
+        src="/Giornale/Razzo.png"
+        class="img"
+        :style="{ transform: `translateY(${y-55}px)` }"
+    />
+    <img
+        v-if="isMoving"
+        src="/Giornale/Fuoco.png"
+        class="img"
+        :style="{ transform: `translateY(${y}px)` }"
+    />
+
   </div>
 
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
-  <br>
+
+
 </template>
 
-<style scoped>
+<style>
+.container {
+  position: relative;
+  height: 70vh;
+  overflow: hidden;
+}
 
+.img {
+  position: absolute;
+  bottom: 0;
+  left: 10%;
+  transform: translateX(-50%);
+}
+
+
+.countdown {
+  text-align: center;
+  font-size: 40px;
+}
 </style>
