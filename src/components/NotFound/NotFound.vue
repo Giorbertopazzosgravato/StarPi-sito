1bb23f479903fd086c548febcc91277fb4b6b77f
scar <riccardo.scaletta05@gmail.com>
2026-03-31 15:49:42 +0200

    aggiunta bomba alla pagina 404

--- src/components/NotFound/NotFound.vue
+++ src/components/NotFound/NotFound.vue
@@ -2,9 +2,8 @@
   import { ref, onMounted } from 'vue'
 
   const y = ref(0)              // vertical position
-  document.addEventListener("DOMContentLoaded", () => {
-    const slide = document.getElementById("putenza");
-    console.log(slide.value);
+
+
     function sleep(ms) {
       return new Promise(r => setTimeout(r, ms))
     }
@@ -29,10 +28,19 @@
     onMounted(() => {
       // start at bottom
       y.value = window.innerHeight - 900
+
+
+      while(true) {
+        const slider = document.getElementById("putenza")
+        console.log(slider.value)
+          sleep(1000);
+      }
+
       startSequence()
     })
 
-  });
+
+
 
 
 </script>
@@ -48,31 +56,35 @@
 
 
 
+  <div>
+    <div style="text-align: center">
+      <h1> Ma che stai a di fra no davvero FR la pagina un c'è </h1>
+      <h2 > →→    trascina per tornare alla home   →→ </h2>
+      <input type="range" id="putenza" name="putenza" min="0" max="10" value="0" />
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <router-link to="/home" style="color: #FF6200"> clicca qui per tornare alla pagina principale </router-link>
+    </div>
 
-  <h1 style="text-align: center"> Ma che stai a di fra no davvero FR la pagina un c'è </h1>
-
-
-
-
-  <div style="text-align: center">
-
-
-
-     <router-link to="/home" style="color: #FF6200"> torna alla pagina principale </router-link>
   </div>
 
-  <!--
 
 
-  <h2> →→    trascina per tornare alla home   →→ </h2>
-    <input type="range" id="putenza" name="putenza" min="0" max="10" value="0" />
 
-
+  <div>
     <img
         src="/Giornale/Razzo.png"
         class="img"
         :style="{ transform: `translateY(${y-55}px)` }"
-    /> -->
+    />
+  </div>
+
+
+
   </template>
 
 
